22370d4cd6ae63732e814596cdfb8e3c40dcb2f4
Zeus <koenma413@sina.com>
2018-07-30 09:17:48 +0800

    add 实心菱形

--- src/day3/Employee.java
+++ src/day3/Employee.java
@@ -2,51 +2,98 @@ package day3;
 
 public class Employee {
 
-    private String name;
+    public static final String COMPANY_NAME = "hell";
+    /* 属于类,只有一个,用于内部累加id,在各个对象中没有拷贝 */
+    private static int nextid;
+
+    private String name = "";
     private int age;
     private String designation;
     private double salary;
-
-    private static final String company_name = "hell";
-    private static int id;
+    private int id  = currentID();
 
     // 构造器
     public Employee() {
-        this.name = "runoob";
-        id++;
     }
 
     // Employee 类的构造器
     public Employee(String name) {
         this.name = name;
         this.age = 99;
-        id++;
     }
+
     // 多个构造器
     public Employee(String name, int age) {
         this.name = name;
         this.age = age;
-        id++;
+    }
+
+    private static int currentID() {
+        int ret = nextid;
+        nextid++;
+        return ret;
     }
 
     // 设置age的值
-    void empAge(int empAge){
-        age =  empAge;
+    void empAge(int empAge) {
+        age = empAge;
     }
+
     /* 设置designation的值*/
-    void empDesignation(String empDesig){
+    void empDesignation(String empDesig) {
         designation = empDesig;
     }
+
     /* 设置salary的值*/
-    void empSalary(double empSalary){
+    void empSalary(double empSalary) {
         salary = empSalary;
     }
+
+    // 没有定义private,public,可以被包中其他方法访问
+    void empSetInfo(int age) {
+        this.age = age;
+    }
+
+    // 方法重载,方法的签名不包括放回值
+    void empSetInfo(String name,
+                    int age,
+                    String designation,
+                    double salary) {
+        this.name = name;
+        this.age = age;
+        this.designation = designation;
+        this.salary = salary;
+    }
+
     /* 打印信息 */
-    void printEmployee(){
-        System.out.println("名字:"+ name );
-        System.out.println("年龄:" + age );
-        System.out.println("职位:" + designation );
+    void printEmployee() {
+        System.out.println("名字:" + name);
+        System.out.println("年龄:" + age);
+        System.out.println("职位:" + designation);
         System.out.println("薪水:" + salary);
         System.out.println("ID:" + id);
+    }
+
+    // 静态方法
+    public static int getID() {
+        return nextid;
+    }
+
+    /**
+     * @brief 用于单元测试,单元测试时会运行,非单元测试时候不运行
+     * @param args
+     */
+    public static void main(String[] args) {
+        Employee empZero = new Employee();
+        Employee empOne = new Employee("RUNOOB1");
+        Employee empTwo = new Employee("RUNOOB2");
+        Employee empThree = new Employee("RUNOOB3", 29);
+
+        System.out.println("----------\r");
+        empZero.printEmployee();
+        empOne.printEmployee();
+        empTwo.printEmployee();
+        empThree.printEmployee();
+        System.out.println("----------\r");
     }
 }
--- src/day3/EmployeeTest.java
+++ src/day3/EmployeeTest.java
@@ -1,21 +1,36 @@
-package day3;
+package day3;  //将下述类放入 day3包中
 
-//import day3.Employee;
 
-public class EmployeeTest{
+import static java.lang.System.out;
+import static java.lang.System.setOut;
 
-    public static void main(String []args){
-        /* 使用构造器创建两个对象 */
+
+public class EmployeeTest {
+
+    public static void main(String[] args) {
+        /* 使用构造器创建四个对象 */
+        // 构造器 重载
+        System.out.println("----------");
         Employee empZero = new Employee();
-        Employee empOne = new Employee("RUNOOB1");
-        Employee empTwo = new Employee("RUNOOB2");
-        Employee empThree = new Employee("RUNOOB3", 29);
-
-        System.out.println("----------\r");
         empZero.printEmployee();
-        ,
+        System.out.println("----------");
+        Employee empOne = new Employee("RUNOOB1");
+        empOne.printEmployee();
+        System.out.println("----------");
+        Employee empTwo = new Employee("RUNOOB2");
+        empTwo.printEmployee();
+        System.out.println("----------");
+        Employee empThree = new Employee("RUNOOB3", 29);
+        empThree.printEmployee();
+        System.out.println("----------");
+        Employee empfour = new Employee();
+        empfour.printEmployee();
+        System.out.println("----------");
 
         // 调用这两个对象的成员方法
+        empZero.empSetInfo(77);
+        empZero.printEmployee();
+
         empOne.empAge(26);
         empOne.empDesignation("高级程序员");
         empOne.empSalary(1000);
@@ -26,9 +41,90 @@ public class EmployeeTest{
         empTwo.empSalary(500);
         empTwo.printEmployee();
 
-        System.out.println("");
         empThree.printEmployee();
 
+        empfour.empSetInfo(32);
+        empfour.empSetInfo("RUNOOB4", 35, "架构师", 17000.5);
+        empfour.printEmployee();
 
+        System.out.println("----------");
+        int n = Employee.getID();
+        System.out.println("累计人数:" + n);
+        System.out.println("公司名:" + Employee.COMPANY_NAME);
+
+        out.println("-------------------");
+
+
+        int lin = 19;
+
+        if (1 == lin%2) {
+
+            int m = lin/2 + 1;
+            for (int i=0; i<m; i++)
+            {
+                for (int j=0; j<m-i-1; j++)
+                {
+                    System.out.print(" ");
+                }
+
+                for (int k=0; k<(i*2)+1; k++)
+                {
+                    System.out.print("*");
+                }
+
+                System.out.println();
+            }
+
+            int N = lin/2;
+
+            for (int i=0; i<N; i++)
+            {
+                for (int j=0; j<i+1; j++)
+                {
+                    System.out.print(" ");
+                }
+
+                for (int k=0; k<(N-i)*2-1; k++)
+                {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        } else {
+            int m = lin/2;
+            for (int i=0; i<m; i++)
+            {
+                for (int j=0; j<m-i-1; j++)
+                {
+                    System.out.print(" ");
+                }
+
+                for (int k=0; k<((i+1)*2); k++)
+                {
+                    System.out.print("*");
+                }
+
+                System.out.println();
+            }
+
+            int N = lin/2;
+
+            for (int i=0; i<N; i++)
+            {
+                for (int j=0; j<i; j++)
+                {
+                    System.out.print(" ");
+                }
+
+                for (int k=0; k<(N-i)*2; k++)
+                {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        } // if (lin%2 == 0)
     }
 }
+
+
+
