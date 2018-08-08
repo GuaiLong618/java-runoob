d42b76fbdb409c90a38ef6d933f69dcafd0d016e
Zeus <koenma413@sina.com>
2018-08-08 10:48:50 +0800

    add 酒店作业

--- src/day9/Hotel.java
+++ src/day9/Hotel.java
@@ -1,7 +1,7 @@
 package day9;
 
 
-import javax.swing.JOptionPane;
+import javax.swing.*;
 import java.util.Random;
 
 /**
@@ -19,9 +19,12 @@ public class Hotel {
 
         for (int i = 0; i < FLOOR; i++) {
             for (int j = 0; j < ROOM_NUMBER; j++) {
+                Room r = new Room();
+                Random rand = new Random(2);
+                this.rooms[i][j] = r;
                 this.rooms[i][j].status = false;
-                this.rooms[i][j].type = 3;
-                int kk = i*100 + j;
+                this.rooms[i][j].type = rand.nextInt(3);
+                int kk = (i*100 + 1) + j + 1;
                 this.rooms[i][j].no = kk;
             }
         }
@@ -41,12 +44,13 @@ public class Hotel {
             System.out.println(number);
 
             int kk = Integer.parseInt(number);
-            int floorNo = kk / 100;
-            int roomNo = kk % 100;
+            int floorNo = kk / 100 - 1;
+            int roomNo = kk % 100 - 1;
             if ((roomNo <= 10) && (floorNo <= 5)) {
                 if (this.rooms[floorNo][roomNo].status == false) {
                     this.rooms[floorNo][roomNo].status = true;
                     JOptionPane.showMessageDialog(null, "房间预定成功");
+                    break;
                 } else {
                     JOptionPane.showMessageDialog(null,"房间已经被预定");
                 }
@@ -77,10 +81,14 @@ public class Hotel {
     public void printRoomsList() {
         for (int i = 0; i < FLOOR; i++) {
             for (int j = 0; j < ROOM_NUMBER; j++) {
+                int kk = ((i+1)*100) + j + 1;
+                System.out.print(kk + ":");
+                int type = this.rooms[i][j].type;
+                System.out.print(type);
                 if (this.rooms[i][j].status) {
-                    System.out.print("[*]");
+                    System.out.print("[*] ");
                 } else {
-                    System.out.print("[ ]");
+                    System.out.print("[ ] ");
                 }
             }
             System.out.println();
@@ -89,6 +97,19 @@ public class Hotel {
 
     public static void main(String[] args) {
         Hotel hotel = new Hotel();
-        hotel.printRoomsList();
+        while (true) {
+            int jude = 0;
+            jude = JOptionPane.showConfirmDialog(null,"约定?");
+            System.out.println(jude);
+            if (0 == jude) {
+                hotel.makeRoomReservation();
+                hotel.printRoomsList();
+            } else if (1 == jude) {
+                System.exit(0);
+            } else {
+                System.exit(0);
+            }
+
+        }
     }
 }
